e54e67c7a3a4c6af720d40930c54927f03b75d77
joaolucasgozzi <lukasgozzi@gmail.com>
2022-11-18 18:02:35 -0400

    fix: front env bug

--- back/src/app.ts
+++ back/src/app.ts
@@ -3,8 +3,11 @@ import { appRoutes } from "./routes";
 import { errorMiddleware } from "./middlewares/errorHandler.middleware";
 import { Request, Response } from "express";
 import "express-async-errors";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
